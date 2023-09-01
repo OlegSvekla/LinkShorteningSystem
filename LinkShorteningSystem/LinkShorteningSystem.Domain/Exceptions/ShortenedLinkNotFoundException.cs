30d28f69d64091514ab9584882e1d5b0248f025d
OlegSvekla <olegsvekla993@gmail.com>
2023-09-01 17:18:26 +0300

    add GlobalExHandlong, logging to file and rewrote classes names

---
+++ LinkShorteningSystem/LinkShorteningSystem.Domain/Exceptions/ShortenedLinkNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkShorteningSystem.Domain.Exceptions
+{
+    public sealed class ShortenedLinkNotFoundException : Exception
+    {
+        public ShortenedLinkNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
--- LinkShorteningSystem/LinkShorteningSystem/HttpClients/ILinkShorteningSystemHttpClient.cs
+++ LinkShorteningSystem/LinkShorteningSystem/HttpClients/ILinkShorteningSystemHttpClient.cs
@@ -3,6 +3,6 @@
     public interface ILinkShorteningSystemHttpClient
     {
         Task<string> CutLinkAsync(string baseLink, string origin, CancellationToken token = default);
-        Task<string> GetAsync(string baseLink, string shortenedLink, CancellationToken cancellationToken = default);
+        Task<string> GetOriginalByShortendLinkAsync(string baseLink, string shortenedLink, CancellationToken cancellationToken = default);
     }
 }
