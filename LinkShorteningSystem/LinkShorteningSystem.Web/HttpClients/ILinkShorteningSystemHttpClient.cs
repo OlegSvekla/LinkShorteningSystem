5beec6c667ba78058b2d3c2e7a9d20eb118caca0
OlegSvekla <olegsvekla993@gmail.com>
2023-09-01 17:57:00 +0300

    restructure proj

---
+++ LinkShorteningSystem/LinkShorteningSystem.Web/HttpClients/ILinkShorteningSystemHttpClient.cs
@@ -0,0 +1,8 @@
+﻿namespace LinkShorteningSystem.HttpClients
+{
+    public interface ILinkShorteningSystemHttpClient
+    {
+        Task<string> CutLinkAsync(string baseLink, string origin, CancellationToken token = default);
+        Task<string> GetOriginalByShortendLinkAsync(string baseLink, string shortenedLink, CancellationToken cancellationToken = default);
+    }
+}
---
+++ LinkShorteningSystem/LinkShorteningSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinkShorteningSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
