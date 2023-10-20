48edbd1bb1bf41a1951d8e45848b46bbba176166
OlegSvekla <olegsvekla993@gmail.com>
2023-10-20 17:47:43 +0300

    z

--- LinkShorteningSystem/LinkShorteningSystem/HttpClients/ILinkShorteningSystemHttpClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LinkShorteningSystem.HttpClients
-{
-    public interface ILinkShorteningSystemHttpClient
-    {
-        Task<string> CutLinkAsync(string baseLink, string origin, CancellationToken token = default);
-        Task<string> GetOriginalByShortendLinkAsync(string baseLink, string shortenedLink, CancellationToken cancellationToken = default);
-    }
-}
